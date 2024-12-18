93cd1c6f849c8633e314358f01b8452a9a9a5352
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-18 14:55:40 +0530

    Add initial project and user settings files for Unity project setup

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hello World!");
+        print("This is a new script!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
