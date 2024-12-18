4b10943e7d2f223e53455f0739401c361f2eae4f
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-18 15:31:44 +0530

    how to log the data

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         Debug.Log("Welcome to Number Wizard!");
+        // print("Welcome to Number Wizard.....!");
+        Debug.Log("Pick a number in your head, but don't tell me!");
+        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is 1");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
