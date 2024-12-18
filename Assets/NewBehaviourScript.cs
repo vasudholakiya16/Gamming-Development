7b4978081b52d8f7a7b965a8f8a9e3f22ad39e0e
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-18 16:15:14 +0530

    add player input

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -12,12 +12,32 @@ public class NewBehaviourScript : MonoBehaviour
         Debug.Log("Pick a number in your head, but don't tell me!");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
+
+        Debug.Log("Tell me if your number is higher or lower than  500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+
+        // player presses Enter key
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
+        
         
     }
 }
