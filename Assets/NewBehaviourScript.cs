1c4c32f6a01ac46ad37c1084c857b26a4b3e1834
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-12-18 15:47:47 +0530

    Refactor welcome messages and number range in Number Wizard

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,11 +5,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        int max = 1000;
+        int min = 1;
          Debug.Log("Welcome to Number Wizard!");
         // print("Welcome to Number Wizard.....!");
         Debug.Log("Pick a number in your head, but don't tell me!");
-        Debug.Log("The highest number you can pick is 1000");
-        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
         
     }
 
--- Assets/VariableIntro.cs
+++ Assets/VariableIntro.cs
@@ -5,6 +5,13 @@ public class VariableIntro : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        int max = 1000;
+        string welcomeMessage = "Welcome to Number Wizard!";
+        float pi = 3.14159f;
+        bool isGameStarted = true;
+        Debug.Log(welcomeMessage);
+        
+
         
     }
 
